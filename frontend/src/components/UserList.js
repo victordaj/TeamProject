8f6db831bad2f0dca774b3ae8018b76284dd5124
Daj Victor <victordaj7@gmail.com>
2020-11-27 21:36:40 +0200

    User List added

---
+++ frontend/src/components/UserList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class UserList  extends React.Component {
+
+    render() { 
+        return (<div>
+            <h1>
+                User List !!!!
+            </h1>
+            <ul>
+                //map here the list 
+            </ul>
+        </div>);
+    }
+}
+export default UserList
