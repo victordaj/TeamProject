c8225ce3d1d7f09c4ba81053bdc8937404c18ea6
Daj Victor <victordaj7@gmail.com>
2020-11-27 13:05:30 +0200

    Added database model and connection

---
+++ backend/src/utils/database/models/users.js
@@ -0,0 +1,11 @@
+const collection = require('mongoose');
+
+const Users = collection.model('Users',{
+    firstName: String,
+    lastName: String,
+    birthday: Date,
+    isActive: Boolean,
+    password: String
+})
+
+module.exports = Users 
