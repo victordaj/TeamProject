12fcda65b531d21c74ead2cdc3f45ed3f6d0182f
Daj Victor <victordaj7@gmail.com>
2020-12-01 14:13:29 +0200

    Moved functions from App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,76 +1,15 @@
-import CreateUser from './components/CreateUser';
-import UserList from './components/UserList'
+import UserContainer from "./components/UserContainer";
 import React from 'react';
 import ReactDOM from 'react-dom';
-import axios from './utils/axios';
 
-class UserContainer extends React.Component {
 
-  state = {
-    userInput: []
-  }
-
-  error = err => {
-    console.log(err);
-    alert("Error");
-  }
-
-  getUsers = () => {
-    axios.get('/users').then(response => {
-      this.setState({userInput: response.data})
-    }).catch(err => {
-      this.error(err)
-    })
-  }
-
-  deleteUser = userId => {
-    if(window.confirm("Are you sure?")) {
-      axios.put('/users/' + userId, { isActive: false}).then(() => {
-        this.getUsers();
-      }).catch(err => {
-        this.error(err);
-      })
-    }
-  }
-
-  createUser = user => {
-    if(user.firstName && user.lastName && user.password && user.birthday) {
-      axios.post('/users', user).then(() => {
-        this.getUsers();
-      }).catch(err => {
-        this.error(err);
-      })
-    } else alert("Please complete all the fields")
-  }
-
-  updateUser = userId => {
-    let firstName = window.prompt("First name");
-    let lastName = window.prompt("Last name");
-    let password = window.prompt("Password")
-    if(firstName && lastName && password) {
-      axios.put('/users/' + userId, {
-        firstName: firstName, 
-        lastName: lastName, 
-        password: password }).then(() => {
-          this.getUsers();
-        }).catch(err => {
-          this.error(err);
-        })
-    } else alert("Please complete all the fields")
-  }
-
-  componentDidMount() {
-    this.getUsers();
-  }
-
+class App extends React.Component {
   render() {
-    return (
-      <div>
-        <CreateUser onCreate={this.createUser} />
-        <UserList list={this.state.userInput} onDelete={this.deleteUser} onUpdate={this.updateUser} />
-      </div>
+     return (
+       <div className="App">
+           <UserContainer  />
+       </div>
     )
   }
 }
-
-export default UserContainer;
+export default App;
---
+++ frontend/src/components/UserContainer.js
@@ -0,0 +1,76 @@
+import CreateUser from './CreateUser';
+import UserList from './UserList';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from './utils/axios';
+
+class UserContainer extends React.Component {
+
+  state = {
+    userInput: []
+  }
+
+  error = err => {
+    console.log(err);
+    alert("Error");
+  }
+
+  getUsers = () => {
+    axios.get('/users').then(response => {
+      this.setState({userInput: response.data})
+    }).catch(err => {
+      this.error(err)
+    })
+  }
+
+  deleteUser = userId => {
+    if(window.confirm("Are you sure?")) {
+      axios.put('/users/' + userId, { isActive: false}).then(() => {
+        this.getUsers();
+      }).catch(err => {
+        this.error(err);
+      })
+    }
+  }
+
+  createUser = user => {
+    if(user.firstName && user.lastName && user.password && user.birthday) {
+      axios.post('/users', user).then(() => {
+        this.getUsers();
+      }).catch(err => {
+        this.error(err);
+      })
+    } else alert("Please complete all the fields")
+  }
+
+  updateUser = userId => {
+    let firstName = window.prompt("First name");
+    let lastName = window.prompt("Last name");
+    let password = window.prompt("Password")
+    if(firstName && lastName && password) {
+      axios.put('/users/' + userId, {
+        firstName: firstName, 
+        lastName: lastName, 
+        password: password }).then(() => {
+          this.getUsers();
+        }).catch(err => {
+          this.error(err);
+        })
+    } else alert("Please complete all the fields")
+  }
+
+  componentDidMount() {
+    this.getUsers();
+  }
+
+  render() {
+    return (
+      <div>
+        <CreateUser onCreate={this.createUser} />
+        <UserList list={this.state.userInput} onDelete={this.deleteUser} onUpdate={this.updateUser} />
+      </div>
+    )
+  }
+}
+
+export default UserContainer;
